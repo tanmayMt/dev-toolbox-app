fb55bf3a99448b11f1e3f49291e354166aa62574
Tanmay Samanta <137848052+tanmayMt@users.noreply.github.com>
2025-07-22 10:12:44 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,20 +3,26 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const jsonRoutes = require("./routes/json.routes");
 const base64Routes = require("./routes/base64.routes");
+require("dotenv").config();
 
 const app = express();
 // app.use(cors());
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: `http://localhost:3000`,
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Dev Toolbox API</h1>");
+});
+
 app.use("/api/json", jsonRoutes);
 app.use("/api/base64", base64Routes);
 
 module.exports = app;
+
